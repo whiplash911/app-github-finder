44e36e785e0f2b97cf58bfe31f8772d4ae2c105f
Doncho Filipov <doncho.filipov@scalefocus.com>
2019-10-23 14:04:13 +0300

    Home and NotFound components.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React, { useState, Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Alert from './components/layout/Alert';
-import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 import About from './components/pages/About';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
 
 const App = () => {
-  const [alert, setAlert] = useState(null);
-
-  // Method that displays an alert
-  const showAlert = (message, alertType) => {
-    setAlert({ msg: message, type: alertType });
-    setTimeout(() => setAlert(null), 5000);
-  };
-
   return (
     <GithubState>
       <AlertState>
@@ -27,20 +19,11 @@ const App = () => {
             <Navbar />
             <div className='container'>
               <Alert />
-
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={props => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:username' component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
